065d7685f68a3094138fab5b20bffb7019a74507
btdev0306 <btdev0306@outlook.com>
2024-05-02 13:57:07 -0400

    update

--- mypracticeproject/src/main.rs
+++ mypracticeproject/src/main.rs
@@ -72,10 +72,24 @@ fn pro4(){
     list.sort_by_key(|r| r.width);
     println!("{:#?}",list);
 }
+fn pro5(){
+    let mut list=[
+        Rectangle{width:10, height:1},
+        Rectangle{width:3, height:5},
+        Rectangle{width:7, height:12},
+    ];
+    let mut num_sort_operations=0;
+    list.sort_by_key(|r|{
+        num_sort_operations+=1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
+}
 fn main() {
     println!("Hello, world!");
     //pro1();
     //pro2();
     //pro3();
-    pro4();
+    //pro4();
+    pro5();
 }
