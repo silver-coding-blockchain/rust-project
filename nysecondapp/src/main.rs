09ce02ee899bec9481a128391753448c1348715e
btdev0306 <btdev0306@outlook.com>
2024-05-02 02:26:39 -0400

    practice

--- nysecondapp/src/main.rs
+++ nysecondapp/src/main.rs
@@ -51,7 +51,32 @@ fn pro1(){
     println!("{s3}  ");
     println!("{s4}");
 }
+fn pro2(){
+    use std::collections::HashMap;
+    let text="hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace(){
+        let count=map.entry(word).or_insert(0);
+        *count+=1;
+    }
+    println!("{:?}",map);
+}
+fn pro3(){
+    use std::fs::File;
+    use std::io::ErrorKind;
+    let greeting_file=File::open("hello.txt").unwrap_or_else(|error|{
+        if error.kind()==ErrorKind::NotFound{
+            File::create("hello.txt").unwrap_or_else(|error|{
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else{
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
 fn main(){
     //pro_using_enum_to_store_multipleTypes();
-    pro1();
+    //pro1();
+    //pro2();
+    pro3();
 }
