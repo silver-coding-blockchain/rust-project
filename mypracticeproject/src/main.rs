dd8c287d41b646b5c126840a883c21faa3f854f2
btdev0306 <btdev0306@outlook.com>
2024-05-02 11:39:27 -0400

    practice

--- mypracticeproject/src/main.rs
+++ mypracticeproject/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
+fn pro1(){
+    let file_path="poem.txt";
+    println!("In file {}", file_path);
+    let contents=fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
+}
 fn main() {
     println!("Hello, world!");
+    pro1();
 }
