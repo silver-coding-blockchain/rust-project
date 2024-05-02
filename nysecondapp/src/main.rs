b291f85948246afc610d43eef2d8a73473ac93b1
btdev0306 <btdev0306@outlook.com>
2024-05-01 21:31:50 -0400

    update1

--- nysecondapp/src/main.rs
+++ nysecondapp/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod back_of_house{
+    pub struct Breakfast{
+        pub toast:String,
+        seasonal_fruit:String,
+    }
+    impl Breakfast{
+        pub fn summer(toast:&str)->Breakfast{
+            Breakfast{
+                toast:String::from(toast),
+                seasonal_fruit:String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant(){
+    let mut meal=back_of_house::Breakfast::summer("Rye");
+    meal.toast=String::from("wheat");
+    println!("I'd like {} toast please", meal.toast);
+}
+fn main(){
+    eat_at_restaurant();
 }
