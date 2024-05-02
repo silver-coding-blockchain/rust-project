ce42de637b9bdb37e0e5e9ae2662b0f7b78ae2d2
btdev0306 <btdev0306@outlook.com>
2024-05-02 10:14:46 -0400

    pracitce

--- nysecondapp/src/main.rs
+++ nysecondapp/src/main.rs
@@ -186,6 +186,34 @@ mod tests{
         panic!("Make this test fail");
     }
 }
+
+//#[derive(Debug)]
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+impl Rectangle{
+    fn can_hold(&self, other:&Rectangle)->bool {
+        self.width>other.width&&self.height>other.height
+    }
+}
+#[cfg(test)]
+mod tests1{
+    use super::*;
+    #[test]
+    fn larger_can_hold_smaller(){
+        let larger=Rectangle{
+            width:8,
+            height:7,
+        };
+        let smaller=Rectangle{
+            width:5,
+            height:1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+}
+
 fn main(){
     //pro_using_enum_to_store_multipleTypes();
     //pro1();
