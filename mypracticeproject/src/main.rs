079404190f49fe55542cc3d895066dd6927fda20
btdev0306 <btdev0306@outlook.com>
2024-05-02 13:17:01 -0400

    update

--- mypracticeproject/src/main.rs
+++ mypracticeproject/src/main.rs
@@ -1,6 +1,33 @@
 use std::env;
 use std::fs;
-
+#[derive(Debug,PartialEq,Copy,Clone)]
+enum ShirtColor{
+    Red,
+    Blue,
+}
+struct Inventory{
+    shirts:Vec<ShirtColor>,
+}
+impl Inventory{
+    fn giveway(&self, user_preference:Option<ShirtColor>)->ShirtColor{
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+    fn most_stocked(&self)->ShirtColor{
+        let mut num_red=0;
+        let mut num_blue=0;
+        for color in &self.shirts{
+            match color{
+                ShirtColor::Red=>num_red+=1,
+                ShirtColor::Blue=>num_blue+=1,
+            }
+        }
+        if num_red>num_blue{
+            ShirtColor::Red
+        } else{
+            ShirtColor::Blue
+        }
+    }
+}
 fn pro1(){
     let file_path="poem.txt";
     println!("In file {}", file_path);
@@ -8,7 +35,23 @@ fn pro1(){
         .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 }
+fn pro2(){
+    let store=Inventory{
+        shirts:vec![ShirtColor::Blue,ShirtColor::Red, ShirtColor::Blue],
+    };
+    let user_pref1=Some(ShirtColor::Red);
+    let giveway1=store.giveway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}", user_pref1, giveway1
+    );
+    let user_pref2=None;
+    let giveway2=store.giveway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}", user_pref2, giveway2
+    );    
+}
 fn main() {
     println!("Hello, world!");
-    pro1();
+    //pro1();
+    pro2();
 }
