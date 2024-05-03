a1f2da9a95923fc4e45ff62f77b4b5adf271299a
btdev0306 <btdev0306@outlook.com>
2024-05-03 12:07:04 -0400

    update

--- mypracticeproject/src/main.rs
+++ mypracticeproject/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::btree_map::Values;
 use std::env;
 use std::fs;
 use std::fs::Metadata;
+use std::pin;
 use std::rc::Rc;
 use std::thread;
 use std::time::Duration;
@@ -254,6 +255,68 @@ fn pro13(){
     collection.add(101);
     println!("average={}",collection.average);
 }
+pub struct Post{
+    state:Option<Box<dyn State>>,
+    content:String,
+}
+impl Post{
+    pub fn new()->Post{
+        Post { state: Some(Box::new(Draft{})), content: String::new(), }
+    }
+    pub fn add_text(&mut self, text:&str){
+        self.content.push_str(text);
+    }
+    pub fn content(&self)->&str{
+        self.state.as_ref().unwrap().content(self)
+    }
+    pub fn request_review(&mut self){
+        if let Some(s)=self.state.take(){
+            self.state=Some(s.request_review())
+        }
+    }
+}
+trait State{
+    fn request_review(self:Box<Self>)->Box<dyn State>;
+    fn approve(self:Box<Self>)->Box<dyn State>;
+    fn content<'a>(&self,post:&'a Post)->&'a str{
+        ""
+    }
+}
+struct Draft{}
+struct Published{}
+struct PendingReview{}
+impl  State for Draft {
+    fn request_review(self:Box<Self>)->Box<dyn State> {
+        Box::new(PendingReview{})
+    }
+    fn approve(self:Box<Self>)->Box<dyn State> {
+        self
+    }
+}
+impl State for PendingReview {
+    fn request_review(self:Box<Self>)->Box<dyn State> {
+        self
+    }
+    fn approve(self:Box<Self>)->Box<dyn State> {
+        Box::new(Published{})
+    }
+}
+impl State for Published {
+    fn request_review(self:Box<Self>)->Box<dyn State> {
+        self
+    }
+    fn approve(self:Box<Self>)->Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self,post:&'a Post)->&'a str {
+        &post.content
+    }
+}
+fn pro14(){
+    let mut post=Post::new();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+}
 fn main() {
     println!("Hello, world!");
     //pro1();
@@ -268,6 +331,7 @@ fn main() {
     //pro10();
     //pro11();
     //pro12();
-    pro13();
+    //pro13();
+    pro14();
 }
 
