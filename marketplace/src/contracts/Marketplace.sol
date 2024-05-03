3d0e583fde0cfbfa0a2647541eca6526be30b86d
btdev0306 <btdev0306@outlook.com>
2024-05-02 22:41:26 -0400

    update

--- marketplace/src/contracts/Marketplace.sol
+++ marketplace/src/contracts/Marketplace.sol
@@ -12,7 +12,21 @@ contract Marketplace{
         address owner;
         bool purchased;
     }
+    event ProductCreated{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
     constructor() public{
         name="Dapp University Marketplace";
+    }
+    function createProduct(string memory _name, uint _price) public{
+        require(bytes(_name).length>0);
+        require(_price>0);
+        productCount++;
+        products[productCount]=Product(productCount,_name,_price,msg.sender,false);
+        emit ProductCreated(productCount,_name,_price,msg.sender,false);
     }
 }
