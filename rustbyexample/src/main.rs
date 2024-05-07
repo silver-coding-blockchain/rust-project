b12e5f6be841e7a9d3aa0fc38364738c1be10c3f
btdev0306 <btdev0306@outlook.com>
2024-05-07 07:27:07 -0400

    add

--- rustbyexample/src/main.rs
+++ rustbyexample/src/main.rs
@@ -1,8 +1,10 @@
 mod Formatting;
 mod primitives_literals_operators;
 mod tuples;
+mod array_and_slice;
 fn main() {
    //Formatting::main();
    //primitives_literals_operators::main();
-   tuples::main();
+   //tuples::main();
+   array_and_slice::main();
 }
