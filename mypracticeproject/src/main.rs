5d14f2836afbcc6ad8baa09632d2d78e13f40142
btdev0306 <btdev0306@outlook.com>
2024-05-10 08:08:06 -0400

    update

--- mypracticeproject/src/main.rs
+++ mypracticeproject/src/main.rs
@@ -433,6 +433,56 @@ fn pro22(){
     let first_from_b=first_element(&b);
     println!("{first_from_b:?}");
 }
+/// The book type provided by an external API.
+#[derive(Debug)]
+struct APIBook {
+    title: String,
+    description: Option<String>,
+}
+
+/// The book type you need in the rest of your program.
+#[derive(Debug)]
+struct Book {
+    title: String,
+    description: String,
+}
+
+fn pro23() {
+    // The book objects you "received" from an API.
+    let api_books: Vec<APIBook> = vec![
+        APIBook {
+            title: "Samson and Rik".to_string(),
+            description: Some("Samson and Rik go on many adventures.".to_string()),
+        },
+        APIBook {
+            title: "De Kameleon".to_string(),
+            description: None,
+        },
+    ];
+
+    println!("api_books: {api_books:#?}");
+
+    // The book objects you would like to use throughout the rest of your program.
+    let books: Vec<Book> = api_books
+        .into_iter()
+        .filter_map(|api_book| {
+            // Deconstruct the APIBook into its parts.
+            let APIBook { title, description } = api_book;
+
+            // Return None if description is None, otherwise take the String out of the `Option<String>`.
+            let description = match description {
+                Some(description) => description,
+                None => return None,
+            };
+
+            // Create Book from the parts.
+            Some(Book { title, description })
+        })
+        .collect::<Vec<_>>();
+
+    println!("books: {books:#?}");
+}
+
 fn main() {
     println!("Hello, world!");
     //pro1();
@@ -452,6 +502,6 @@ fn main() {
     //pro15();
     //pro16();
     //pro17();
-    pro22();
+    pro23();
 }
 
