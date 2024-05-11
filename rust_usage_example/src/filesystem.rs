0abdd3da6352558fcb438e3b1021152cd3db936d
btdev0306 <btdev0306@outlook.com>
2024-05-11 09:15:43 -0400

    update

--- rust_usage_example/src/filesystem.rs
+++ rust_usage_example/src/filesystem.rs
@@ -1,17 +1,17 @@
-#![allow(unused)]
 use std::{fs, io};
 
-pub fn main() {
-    let mut entries = fs::read_dir(".")
-        .expect("That dir cannot open");
-    for entry in entries{
-        let file_type=entry.file_type()?;
-        let prefix=match file_type{
+pub fn main() -> io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        let prefix = match file_type {
             t if t.is_dir() => "D",
             t if t.is_file() => "F",
             t if t.is_symlink() => "L",
             _ => "?",
-        }
-    }    
+        };
+        println!("{prefix} {}", entry.path().display());
+    }
 
+    Ok(())
 }
--- rust_usage_example/src/main.rs
+++ rust_usage_example/src/main.rs
@@ -1,14 +1,4 @@
-use std::{fs, io};
-fn main() {
-    for entry in fs::read_dir(".")? {
-        let entry = entry?;
-        let file_type = entry.file_type()?;
-        let prefix = match file_type {
-            t if t.is_dir() => "D",
-            t if t.is_file() => "F",
-            t if t.is_symlink() => "L",
-            _ => "?",
-        };
-        println!("{prefix} {}", entry.path().display());
-    }
+mod filesystem;
+fn main(){
+    let _result=filesystem::main();
 }
