3e01e70780f42e91cc3a1879644136a21976265d
btdev0306 <btdev0306@outlook.com>
2024-05-20 08:17:48 -0400

    update

--- rustbyexample/src/main.rs
+++ rustbyexample/src/main.rs
@@ -9,7 +9,8 @@
 //mod constants;
 //mod mutability;
 //mod scope_and_shadowing;
-mod casting;
+//mod casting;
+mod ifandelse;
 fn main() {
     // Formatting::main();
     // primitives_literals_operators::main();
@@ -17,6 +18,7 @@ fn main() {
     //array_and_slice::main();
     //scope_and_shadowing::main1()  ;
     //scope_and_shadowing::main2()  ;
-    casting::main();
+    //casting::main();
+    ifandelse::main();
 
 }
