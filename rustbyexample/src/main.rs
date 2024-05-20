7e9448068fb31bad6ff95d8ad50e88d1b028ee3b
btdev0306 <btdev0306@outlook.com>
2024-05-20 08:13:12 -0400

    update

--- rustbyexample/src/main.rs
+++ rustbyexample/src/main.rs
@@ -8,12 +8,15 @@
 //mod enums;
 //mod constants;
 //mod mutability;
-mod scope_and_shadowing;
+//mod scope_and_shadowing;
+mod casting;
 fn main() {
     // Formatting::main();
     // primitives_literals_operators::main();
     // tuples::main();
     //array_and_slice::main();
-    scope_and_shadowing::main1()  ;
-    scope_and_shadowing::main2()  ;
+    //scope_and_shadowing::main1()  ;
+    //scope_and_shadowing::main2()  ;
+    casting::main();
+
 }
