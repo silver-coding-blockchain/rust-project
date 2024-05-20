823cc748851aa8e1e093d08b15c390e22f6d0468
btdev0306 <btdev0306@outlook.com>
2024-05-20 08:21:14 -0400

    update

--- rustbyexample/src/main.rs
+++ rustbyexample/src/main.rs
@@ -10,7 +10,8 @@
 //mod mutability;
 //mod scope_and_shadowing;
 //mod casting;
-mod ifandelse;
+//mod ifandelse;
+mod nesting_labels_loop;
 fn main() {
     // Formatting::main();
     // primitives_literals_operators::main();
@@ -19,6 +20,7 @@ fn main() {
     //scope_and_shadowing::main1()  ;
     //scope_and_shadowing::main2()  ;
     //casting::main();
-    ifandelse::main();
+    //ifandelse::main();
+    nesting_labels_loop::main();
 
 }
